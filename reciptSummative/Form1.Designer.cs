fa6935aeab675b5db288d05fae0a6d06190c42b9
jordsinc828@ed.amdsb.ca <jordsinc828@ed.amdsb.ca>
2018-10-11 13:46:56 -0400

    Add project files.

---
+++ reciptSummative/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace reciptSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itemOneLabel = new System.Windows.Forms.Label();
+            this.itemTwoLabel = new System.Windows.Forms.Label();
+            this.itemThreeLabel = new System.Windows.Forms.Label();
+            this.itemOneBox = new System.Windows.Forms.TextBox();
+            this.itemTwoBox = new System.Windows.Forms.TextBox();
+            this.itemThreebox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subTotalAmount = new System.Windows.Forms.Label();
+            this.taxAmountLabel = new System.Windows.Forms.Label();
+            this.totalAmountLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.changeAmount = new System.Windows.Forms.Button();
+            this.reciptButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeAmountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // itemOneLabel
+            // 
+            this.itemOneLabel.AutoSize = true;
+            this.itemOneLabel.Location = new System.Drawing.Point(43, 23);
+            this.itemOneLabel.Name = "itemOneLabel";
+            this.itemOneLabel.Size = new System.Drawing.Size(87, 13);
+            this.itemOneLabel.TabIndex = 0;
+            this.itemOneLabel.Text = "Number of item 1";
+            // 
+            // itemTwoLabel
+            // 
+            this.itemTwoLabel.AutoSize = true;
+            this.itemTwoLabel.Location = new System.Drawing.Point(43, 65);
+            this.itemTwoLabel.Name = "itemTwoLabel";
+            this.itemTwoLabel.Size = new System.Drawing.Size(87, 13);
+            this.itemTwoLabel.TabIndex = 1;
+            this.itemTwoLabel.Text = "Number of item 2";
+            // 
+            // itemThreeLabel
+            // 
+            this.itemThreeLabel.AutoSize = true;
+            this.itemThreeLabel.Location = new System.Drawing.Point(43, 106);
+            this.itemThreeLabel.Name = "itemThreeLabel";
+            this.itemThreeLabel.Size = new System.Drawing.Size(87, 13);
+            this.itemThreeLabel.TabIndex = 2;
+            this.itemThreeLabel.Text = "Number of item 3";
+            // 
+            // itemOneBox
+            // 
+            this.itemOneBox.Location = new System.Drawing.Point(136, 20);
+            this.itemOneBox.Name = "itemOneBox";
+            this.itemOneBox.Size = new System.Drawing.Size(100, 20);
+            this.itemOneBox.TabIndex = 3;
+            // 
+            // itemTwoBox
+            // 
+            this.itemTwoBox.Location = new System.Drawing.Point(136, 65);
+            this.itemTwoBox.Name = "itemTwoBox";
+            this.itemTwoBox.Size = new System.Drawing.Size(100, 20);
+            this.itemTwoBox.TabIndex = 4;
+            // 
+            // itemThreebox
+            // 
+            this.itemThreebox.Location = new System.Drawing.Point(136, 103);
+            this.itemThreebox.Name = "itemThreebox";
+            this.itemThreebox.Size = new System.Drawing.Size(100, 20);
+            this.itemThreebox.TabIndex = 5;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(96, 145);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // subLabel
+            // 
+            this.subLabel.AutoSize = true;
+            this.subLabel.Location = new System.Drawing.Point(43, 197);
+            this.subLabel.Name = "subLabel";
+            this.subLabel.Size = new System.Drawing.Size(53, 13);
+            this.subLabel.TabIndex = 7;
+            this.subLabel.Text = "Sub Total";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(43, 230);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(25, 13);
+            this.taxLabel.TabIndex = 8;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(43, 259);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 9;
+            this.totalLabel.Text = "Total";
+            // 
+            // subTotalAmount
+            // 
+            this.subTotalAmount.AutoSize = true;
+            this.subTotalAmount.Location = new System.Drawing.Point(136, 197);
+            this.subTotalAmount.Name = "subTotalAmount";
+            this.subTotalAmount.Size = new System.Drawing.Size(65, 13);
+            this.subTotalAmount.TabIndex = 10;
+            this.subTotalAmount.Text = "place holder";
+            this.subTotalAmount.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // taxAmountLabel
+            // 
+            this.taxAmountLabel.AutoSize = true;
+            this.taxAmountLabel.Location = new System.Drawing.Point(136, 230);
+            this.taxAmountLabel.Name = "taxAmountLabel";
+            this.taxAmountLabel.Size = new System.Drawing.Size(65, 13);
+            this.taxAmountLabel.TabIndex = 11;
+            this.taxAmountLabel.Text = "place holder";
+            // 
+            // totalAmountLabel
+            // 
+            this.totalAmountLabel.AutoSize = true;
+            this.totalAmountLabel.Location = new System.Drawing.Point(136, 259);
+            this.totalAmountLabel.Name = "totalAmountLabel";
+            this.totalAmountLabel.Size = new System.Drawing.Size(65, 13);
+            this.totalAmountLabel.TabIndex = 12;
+            this.totalAmountLabel.Text = "place holder";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Location = new System.Drawing.Point(43, 305);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(53, 13);
+            this.tenderedLabel.TabIndex = 13;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // changeAmount
+            // 
+            this.changeAmount.Location = new System.Drawing.Point(96, 341);
+            this.changeAmount.Name = "changeAmount";
+            this.changeAmount.Size = new System.Drawing.Size(75, 23);
+            this.changeAmount.TabIndex = 15;
+            this.changeAmount.Text = "Change";
+            this.changeAmount.UseVisualStyleBackColor = true;
+            // 
+            // reciptButton
+            // 
+            this.reciptButton.Location = new System.Drawing.Point(96, 418);
+            this.reciptButton.Name = "reciptButton";
+            this.reciptButton.Size = new System.Drawing.Size(75, 23);
+            this.reciptButton.TabIndex = 16;
+            this.reciptButton.Text = "Print Recipt";
+            this.reciptButton.UseMnemonic = false;
+            this.reciptButton.UseVisualStyleBackColor = true;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Location = new System.Drawing.Point(43, 386);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(44, 13);
+            this.changeLabel.TabIndex = 17;
+            this.changeLabel.Text = "Change";
+            // 
+            // changeAmountLabel
+            // 
+            this.changeAmountLabel.AutoSize = true;
+            this.changeAmountLabel.Location = new System.Drawing.Point(133, 386);
+            this.changeAmountLabel.Name = "changeAmountLabel";
+            this.changeAmountLabel.Size = new System.Drawing.Size(65, 13);
+            this.changeAmountLabel.TabIndex = 18;
+            this.changeAmountLabel.Text = "place holder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 453);
+            this.Controls.Add(this.changeAmountLabel);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.reciptButton);
+            this.Controls.Add(this.changeAmount);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.totalAmountLabel);
+            this.Controls.Add(this.taxAmountLabel);
+            this.Controls.Add(this.subTotalAmount);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.itemThreebox);
+            this.Controls.Add(this.itemTwoBox);
+            this.Controls.Add(this.itemOneBox);
+            this.Controls.Add(this.itemThreeLabel);
+            this.Controls.Add(this.itemTwoLabel);
+            this.Controls.Add(this.itemOneLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label itemOneLabel;
+        private System.Windows.Forms.Label itemTwoLabel;
+        private System.Windows.Forms.Label itemThreeLabel;
+        private System.Windows.Forms.TextBox itemOneBox;
+        private System.Windows.Forms.TextBox itemTwoBox;
+        private System.Windows.Forms.TextBox itemThreebox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subTotalAmount;
+        private System.Windows.Forms.Label taxAmountLabel;
+        private System.Windows.Forms.Label totalAmountLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button changeAmount;
+        private System.Windows.Forms.Button reciptButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeAmountLabel;
+    }
+}
+
---
+++ reciptSummative/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reciptSummative
+{
+    public partial class Form1 : Form
+    {
+        const double burgerCost = 2.49;
+        const double fryCost = 1.89;
+        const double drinkCost = 0.99;
+        const double taxCost = 0.13;
+        int amountOfBurgers;
+        int amountOfFries;
+        int amountOfDrinks;
+        double totalPrice;
+        double amountTendered;
+        double taxTotal;
+        double changeRequired;
+        double subTotal;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //counting amount of things purchased
+            amountOfBurgers = Convert.ToInt16(itemOneBox.Text);
+            amountOfDrinks = Convert.ToInt16(itemTwoBox.Text);
+            amountOfFries = Convert.ToInt16(itemThreebox.Text);
+
+            //storing costs
+            subTotal = 
+                amountOfFries * fryCost +
+                amountOfDrinks * drinkCost +
+                amountOfBurgers * burgerCost;
+            taxTotal = subTotal * taxCost;
+           
+            //displaying cost
+            subTotalAmount.Text = subTotal + "";
+            taxAmountLabel.Text = subTotal * taxCost + "";
+            totalAmountLabel.Text = taxTotal + subTotal + "";
+
+            
+        }
+    }
+}
